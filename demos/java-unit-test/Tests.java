e925e75563d1b978f10f9c5da05170d5e56cf8e5
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2023-08-25 10:27:22 -0700

    Updating demos/java-unit-test/Tests.java

--- demos/java-unit-test/Tests.java
+++ demos/java-unit-test/Tests.java
@@ -16,7 +16,7 @@ public class Tests {
 
    public double remainder(double a, double b) { return a % b; }
 
-   public double mod(double a, double b) { return a mod b; }
+   public double mod(double a, double b) { return a.mod(b); }
 
   // Add more methods here ...
 }
