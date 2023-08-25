279fb3f770b0019a2ff15be9360a99e36d104118
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2023-08-25 10:36:16 -0700

    Updating demos/java-unit-test/Tests.java

--- demos/java-unit-test/Tests.java
+++ demos/java-unit-test/Tests.java
@@ -16,8 +16,8 @@ public class Tests {
 
    public double remainder(double a, double b) { return a % b; }
 
-
-   public double distance(double x1 = 0; double x2 = 0; double y1 = 0; double y2 = 0; double[] a = {x1,y1}, double[] b = {x2,y2}) { return Math.abs(a - b); }
+double x1 = 0; double x2 = 0; double y1 = 0; double y2 = 0;
+   public double distance(double[] a = {x1,y1}, double[] b = {x2,y2}) { return Math.abs(a - b); }
 
   // Add more methods here ...
 }
