175dc0192a16e0f67bc6d38d8255d50be2d58d46
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2023-08-25 10:23:09 -0700

    Updating demos/java-unit-test/Tests.java

--- demos/java-unit-test/Tests.java
+++ demos/java-unit-test/Tests.java
@@ -2,12 +2,13 @@
  * Just add test methods like the one shown below. (Oh, and maybe fix the bug in
  * subtract.) All numbers are double.
  */
+// Math.abs(-42) &  Math.sqrt(2)
 
 public class Tests {
 
   public double add(double x, double y) { return x + y; }
 
-  public double subtract(double a, double b) { return add(a, b); }
+  public double subtract(double a, double b) { return a - b; }
 
   public double multiply(double a, double b) { return a * b; }
 
