f7215874eadf806fbbe91b56d6962ff0d74e1c70
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2023-09-18 11:22:04 -0700

    Updating assessments/csa-unit-sqrt-2/AssessmentSqrt2.java

--- assessments/csa-unit-sqrt-2/AssessmentSqrt2.java
+++ assessments/csa-unit-sqrt-2/AssessmentSqrt2.java
@@ -13,4 +13,7 @@ public class AssessmentSqrt2 {
    public double distance (double a, double b){
     return (Math.abs(a - b));
   }
+  public double distance2d (double x1, double y1, double x2, double y2){
+    return (Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)));
+  }
 }
