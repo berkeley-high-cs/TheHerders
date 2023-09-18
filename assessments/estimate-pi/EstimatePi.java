5cd8a19d7f99952815ebb030a7f2f597509ac28b
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2023-09-18 11:45:34 -0700

    Updating assessments/estimate-pi/EstimatePi.java

--- assessments/estimate-pi/EstimatePi.java
+++ assessments/estimate-pi/EstimatePi.java
@@ -5,7 +5,7 @@ import java.awt.Graphics;
 public class EstimatePi implements ImageGenerator {
 
   // Increase the value of this variable to get a more precise estimate.
-  int iterations = 100;
+  int iterations = 200;
 
   //////////////////////////////////////////////////////////////////////
   // YOUR CODE GOES HERE - fill out the methods described in the five
