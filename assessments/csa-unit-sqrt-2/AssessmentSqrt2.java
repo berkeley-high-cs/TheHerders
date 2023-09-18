06b9647179b214e97ca0fe829391d0c9653698fb
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2023-09-18 11:36:28 -0700

    Updating assessments/csa-unit-sqrt-2/AssessmentSqrt2.java

--- assessments/csa-unit-sqrt-2/AssessmentSqrt2.java
+++ assessments/csa-unit-sqrt-2/AssessmentSqrt2.java
@@ -31,7 +31,9 @@ public class AssessmentSqrt2 {
   public double toCenter (double itemLength, double space){
     return (Math.abs(1.0/2 * (space - itemLength)));
   }
-  public double randomCoordinate (double val, double start, double length){
+  public double randomCoordinate (){
+    return (Math.random() * 2);
+    public double graphicalCoordinate (double val, double start, double length){
     return (start * (length * (val/2)));
   }
 }
