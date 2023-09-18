cc5b2f94409229469445378a62003f09f45e9731
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2023-09-18 11:28:37 -0700

    Updating assessments/csa-unit-sqrt-2/AssessmentSqrt2.java

--- assessments/csa-unit-sqrt-2/AssessmentSqrt2.java
+++ assessments/csa-unit-sqrt-2/AssessmentSqrt2.java
@@ -17,7 +17,7 @@ public class AssessmentSqrt2 {
     return (Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)));
   }
   public double manhattanDistance (double x1, double y1, double x2, double y2){
-    return (x2 - x1) + (y2 - y1);
+    return (Math.abs(x2 - x1) + Math.abs(y2 - y1));
   }
   public String excited (String word){
     return (word + "!!!");
