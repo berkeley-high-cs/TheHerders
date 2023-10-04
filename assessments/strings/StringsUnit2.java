241fb2425792ad5b8758653f02165d082e8d0dee
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2023-10-04 09:58:33 -0700

    Updating assessments/strings/StringsUnit2.java

--- assessments/strings/StringsUnit2.java
+++ assessments/strings/StringsUnit2.java
@@ -37,7 +37,7 @@ public class StringsUnit2 {
     return (x + "-" + y + "-" + z);
   }
   public String simplePigLatin(String s,int i){
-    String newWord = (s.substring(i, s.length()-1) + s.substring(0,i - 1) + "ay");
+    String newWord = (s.substring(i, s.length()-1) + s.substring(0,i ) + "ay");
     return (newWord);
   }
 }
