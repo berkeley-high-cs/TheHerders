7940193d207c2a6fe44d923fa3e6037a7908771f
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2023-10-04 10:20:51 -0700

    Updating assessments/strings/StringsUnit2.java

--- assessments/strings/StringsUnit2.java
+++ assessments/strings/StringsUnit2.java
@@ -91,7 +91,7 @@ public class StringsUnit2 {
     }
   }
   public String middle(String s){
-  String middle = (s.substring(s.length() / 4, s.length() - (s.length() / 4)));
+  String middle = (s.substring(s.length() / 4, s.length() - (s.length() / 4 ) - 1));
 
     return middle;
   }
