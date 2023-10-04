f0d52fcf39b1b89ca6c858c0c9603ba091c50a7c
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2023-10-04 09:41:55 -0700

    Updating assessments/strings/StringsUnit2.java

--- assessments/strings/StringsUnit2.java
+++ assessments/strings/StringsUnit2.java
@@ -4,4 +4,7 @@ public class StringsUnit2 {
   public String firstCharacter(String s){
     return s.substring(0,1);
   }
+  public String lastCharacter(String s){
+    return s.substring(s.length - 2,s.length - 1);
+  }
 }
