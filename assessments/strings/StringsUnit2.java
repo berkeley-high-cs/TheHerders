3431a2e341db556e6ff5b5e684905d8c17ee277e
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2023-10-04 10:23:56 -0700

    Updating assessments/strings/StringsUnit2.java

--- assessments/strings/StringsUnit2.java
+++ assessments/strings/StringsUnit2.java
@@ -91,7 +91,7 @@ public class StringsUnit2 {
     }
   }
   public String middle(String s){
-  String middle = (s.substring(s.length() / 4, s.length() - (s.length() / 4.0)));
+  String middle = (s.substring(s.length() / 4, s.length() - (s.length() / 4)));
     if (s.equals("food")){
       return "oo";
     }
