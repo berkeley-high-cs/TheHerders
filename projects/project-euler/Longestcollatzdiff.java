53be2e1b4e69051c9956164362e2fbe91273a1af
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2023-10-19 17:38:09 +0000

    works but size problem

--- projects/project-euler/Longestcollatzdiff.java
+++ projects/project-euler/Longestcollatzdiff.java
@@ -1,29 +1,29 @@
 public class Longestcollatzdiff {
     public static void main(String[] args) {
-      int n = 0;
-      int totalChanges = 0;
-      int largestChange = 0;
-      int largestChangeNum = 0;
-       for (int i = 1; i <= 13; i++){
-        if (totalChanges > largestChange){
-            largestChange = totalChanges;
-            largestChangeNum = i - 1;
+        int n = 0;
+        int totalChanges = 0;
+        int largestChange = 0;
+        int largestChangeNum = 0;
+        for (int i = 1; i <= 1000000; i++) {
+            if (totalChanges > largestChange) {
+                largestChange = totalChanges;
+                largestChangeNum = i - 1;
+            }
+            totalChanges = 0;
+            n = i;
+            while (n != 1) {
+                if (n % 2 == 0) {
+                    n = n / 2;
+                } else {
+                    n = 3 * n + 1;
+                }
+                // System.out.println(n);
+                totalChanges++;
+            }
+            // System.out.println(totalChanges);
+            // System.out.println(largestChange + largestChangeNum);
         }
-        totalChanges = 0;
-         n = i;
-         while (n != 1){
-           if (n % 2 == 0){
-             n = n / 2;
-           } else {
-             n = 3 * n + 1;
-           }
-           System.out.println(n);
-           totalChanges++;
-         }
-         //System.out.println(totalChanges);
-         //System.out.println(largestChange + largestChangeNum);
-       }
-       System.out.println(largestChange + " is the biggest amount of change and " 
-       + largestChangeNum + " is the number that goes along with it");
+        System.out.println(largestChange + " is the biggest amount of change and "
+                + largestChangeNum + " is the number that goes along with it");
     }
 }
