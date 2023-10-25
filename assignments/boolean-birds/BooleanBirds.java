96671a3954547396f3ddac209050842c26898cf4
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2023-10-25 09:43:45 -0700

    Updating assignments/boolean-birds/BooleanBirds.java

--- assignments/boolean-birds/BooleanBirds.java
+++ assignments/boolean-birds/BooleanBirds.java
@@ -43,7 +43,7 @@ public class BooleanBirds {
       return (!isFlobbyBird(red, spotted));
   }
   public boolean isRed(String bird){
-    return (bird.equals(Flobby) || bird.equals(Bloggy));
+    return (bird.equals("Flobby") || bird.equals("Bloggy"));
     
   }
 
