e8ac31838e621fbd2ba70c8942cc0692aadb344c
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2023-10-31 09:51:43 -0700

    Updating assignments/primes/Primes.java

--- assignments/primes/Primes.java
+++ assignments/primes/Primes.java
@@ -10,17 +10,17 @@ public class Primes {
     for (int i = 2; i < num; i++){
       if (num % i == 0){
         System.out.println("something false");
-        return false;
+        return true;
       } 
       if (i == num){
         return true;
       }
     }
     System.out.println("something wrong");
-    return false;
+    return true;
   }
   public boolean numberOfPrimesBelow (int num){
-    p
+    
   }
 
 
