061aa8e528d9e8233f5c7f1a426468b7b791f661
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2023-10-31 09:45:59 -0700

    Updating assignments/primes/Primes.java

--- assignments/primes/Primes.java
+++ assignments/primes/Primes.java
@@ -7,7 +7,7 @@
 
 public class Primes {
   public boolean isPrime(int num){
-    for (int i = 0; i <= num/2.0; i++){
+    for (int i = 2; i <= num/2.0; i++){
       if (num % i == 0){
         return false;
       } 
