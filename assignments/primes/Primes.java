c1d9b7725888649882b997ba29014550a6c70545
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2023-10-31 09:49:12 -0700

    Updating assignments/primes/Primes.java

--- assignments/primes/Primes.java
+++ assignments/primes/Primes.java
@@ -10,14 +10,14 @@ public class Primes {
     for (int i = 2; i < num; i++){
       if (num % i == 0){
         System.out.println("something false");
-        //return false;
+        return false;
       } 
       if (i == num){
         return true;
       }
     }
     System.out.println("something wrong");
-    return false;
+    //return false;
   }
 
 
