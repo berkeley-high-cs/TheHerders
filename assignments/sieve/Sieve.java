bafad56e60e2980017ed7afc281358f91ac456d3
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2023-11-13 11:12:53 -0800

    Updating assignments/sieve/Sieve.java

--- assignments/sieve/Sieve.java
+++ assignments/sieve/Sieve.java
@@ -6,4 +6,13 @@ public class Sieve {
     }
     return grid;
   }
+  public int countNonZeros(int[] grid){
+    int total = 0;
+    for (int i = 0; i < grid[].length(); i++){
+      if (grid[i] == 0){
+        total++;
+      }
+    }
+    return total;
+  }
 }
