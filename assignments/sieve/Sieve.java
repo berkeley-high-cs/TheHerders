ec6b1e2fb49ab8aba384f8ba60804ebddb7d8b6a
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2023-11-13 11:09:53 -0800

    Updating assignments/sieve/Sieve.java

--- assignments/sieve/Sieve.java
+++ assignments/sieve/Sieve.java
@@ -1,6 +1,9 @@
 public class Sieve {
   public int[] numberTable(int length){
     int[] grid = new int [length];
+    for (int i = 0; i < length; i ++){
+      grid[i] = i;
+    }
     return grid;
   }
 }
