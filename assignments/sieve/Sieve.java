bcff8c71ba8f9bf8e1b709c855a118726f34d3f3
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2023-11-15 09:57:32 -0800

    Updating assignments/sieve/Sieve.java

--- assignments/sieve/Sieve.java
+++ assignments/sieve/Sieve.java
@@ -1,24 +1,40 @@
 public class Sieve {
   public int[] numberTable(int length){
-    int[] grid = new int [length];
+    int[] array = new int [length];
     for (int i = 0; i < length; i ++){
-      grid[i] = i;
+      array[i] = i;
     }
-    return grid;
+    return array;
   }
-  public int countNonZeros(int[] grid){
+  public int countNonZeros(int[] array){
     int total = 0;
-    for (int i = 0; i < grid.length; i++){
-      if (grid[i] != 0){
+    for (int i = 0; i < array.length; i++){
+      if (array[i] != 0){
         total++;
       }
     }
     return total;
   }
-  public void clearMultiples(int[] grid, int num){
-    for (int i = 0; i < grid.length; i++){
-      if (i % num == 0 && i != num && grid[i] != 0){
-        grid[i] = 0;
+  public void clearMultiples(int[] array, int num){
+    for (int i = 0; i < array.length; i++){
+      if (i % num == 0 && i != num && array[i] != 0){
+        array[i] = 0;
+      }
+    }
+  }
+  public void nonZeros(int[] array){
+    int size = 0;
+    for (int i = 0; i < array.length; i++){
+      if (array[i] != 0){
+        size++;
+      }
+    }
+    int x = 0;
+    int[] nonZeros = new int[size];
+      for (int i = 0; i < array.length; i++){
+      if (array[i] != 0){
+        nonZeros[x] = array[i];
+        x++;
       }
     }
   }
