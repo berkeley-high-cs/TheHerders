617c665574422deaadf2af9a2702a6d9bb0f8375
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2023-11-17 09:48:53 -0800

    Updating assignments/sieve/Sieve.java

---
+++ assignments/sieve/Sieve.java
@@ -0,0 +1,51 @@
+public class Sieve {
+  public int[] numberTable(int length){
+    int[] array = new int [length];
+    for (int i = 0; i < length; i ++){
+      array[i] = i;
+    }
+    return array;
+  }
+  public int countNonZeros(int[] array){
+    int total = 0;
+    for (int i = 0; i < array.length; i++){
+      if (array[i] != 0){
+        total++;
+      }
+    }
+    return total;
+  }
+  public void clearMultiples(int[] array, int num){
+    for (int i = 0; i < array.length; i++){
+      if (i % num == 0 && i != num && array[i] != 0){
+        array[i] = 0;
+      }
+    }
+  }
+  public int[] nonZeros(int[] array){
+    int size = 0;
+    for (int i = 0; i < array.length; i++){
+      if (array[i] != 0){
+        size++;
+      }
+    }
+    int x = 0;
+    int[] nonZeros = new int[size];
+      for (int i = 0; i < array.length; i++){
+      if (array[i] != 0){
+        nonZeros[x] = array[i];
+        x++;
+      }
+    }
+    return nonZeros;
+  }
+   public int nextNonZero(int[] array, int index){
+    
+    for (int i = index + 1; i < array.length; i++){
+      if (array[i] != 0){
+        return array[i];
+      }
+    }
+    return 0;
+  }
+}
