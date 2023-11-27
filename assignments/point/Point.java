8b27cc1580aa8ed39167793ae22c540571ff4593
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2023-11-27 10:55:35 -0800

    Updating assignments/point/Point.java

--- assignments/point/Point.java
+++ assignments/point/Point.java
@@ -1,6 +1,6 @@
 public class Point{
   public static void main(String[] args){
-    System.out.println("hello, world! haha code");
+    System.out.println("hello, world! haha code i can put whatever I want after the hello world");
   }
 
 }
