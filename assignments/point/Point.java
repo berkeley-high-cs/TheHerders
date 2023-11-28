6bc16e7c5c1a16e3185d2a3e3008f7090585b88e
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2023-11-28 09:43:22 -0800

    Updating assignments/point/Point.java

--- assignments/point/Point.java
+++ assignments/point/Point.java
@@ -2,7 +2,7 @@ public class Point{
   private double x;
   private double y;
   public Point(double x, double y){
-    this.x = y;
+    this.x = x;
     this.y = y;
   }
   public static void main(String[] args){
