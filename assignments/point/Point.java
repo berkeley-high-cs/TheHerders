2c9f41f3a87f1be92c580a54ef6994518b61c21e
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2023-11-28 09:39:49 -0800

    Updating assignments/point/Point.java

--- assignments/point/Point.java
+++ assignments/point/Point.java
@@ -1,4 +1,6 @@
 public class Point{
+  private double x;
+  private double y;
   public static void main(String[] args){
     System.out.println("hello, world! haha code i can put whatever I want after the hello world");
   }
