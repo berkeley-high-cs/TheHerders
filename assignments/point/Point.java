c16ef5031ec6d393714d4be3b6f41198596cc7df
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2023-12-01 09:56:19 -0800

    Updating assignments/point/Point.java

--- assignments/point/Point.java
+++ assignments/point/Point.java
@@ -8,5 +8,8 @@ public class Point{
   public static void main(String[] args){
     System.out.println("hello, world! haha code i can put whatever I want after the hello world");
   }
+  public static double distanceFromOrigin(){
+    return Math.hypot(a, b);
+  }
 
 }
