4356ad4e30ba2322a5c590f0ea113978674a7f6d
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2023-12-01 10:28:03 -0800

    Updating assignments/point/Point.java

--- assignments/point/Point.java
+++ assignments/point/Point.java
@@ -8,7 +8,7 @@ public class Point{
   public static void main(String[] args){
     Point newPoint = new Point (300, 400);
 
-    System.out.println("hello, world! p: " + toString(newPoint) + " midpoint: " + toString(midwayTo(newPoint)));
+    //System.out.println("hello, world! p: " + toString(newPoint) + " midpoint: " + toString(midwayTo(newPoint)));
 
   }
   public double distanceFromOrigin(){
