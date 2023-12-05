44c55fa32817447abe396d8659f3af5a7387cff1
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2023-12-05 09:59:17 -0800

    Updating assignments/numbers/Numbers.java

--- assignments/numbers/Numbers.java
+++ assignments/numbers/Numbers.java
@@ -1,27 +1,30 @@
 public class Numbers {
+
   private int[] numbers;
   private int size;
-  public Numbers(int arraySize){
-    this.numbers = new int [arraySize];
+
+  public Numbers(int arraySize) {
+    this.numbers = new int[arraySize];
     this.size = size;
   }
-  public static void main(String[] args){
 
-  }
-  public int size(){
+  public static void main(String[] args) {}
+
+  public int size() {
     return size;
   }
-  public void add(int num){
-    if (numbers.length == size){
+
+  public void add(int num) {
+    if (numbers.length <= size) {
       int[] newArray = new int[numbers.length * 2];
-      for (int i = 0; i <= numbers.length; i++){
+      for (int i = 0; i <= numbers.length; i++) {
         newArray[i] = numbers[i];
       }
       newArray[size] = num;
-
+      size++
     } else {
-    numbers[size] = num;
-    size++;
+      numbers[size] = num;
+      size++;
     }
   }
 }
