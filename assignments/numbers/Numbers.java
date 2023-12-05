c65f23fa3ae67970ad3e20561df433a6c0be9383
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2023-12-05 09:45:28 -0800

    Updating assignments/numbers/Numbers.java

--- assignments/numbers/Numbers.java
+++ assignments/numbers/Numbers.java
@@ -1,6 +1,10 @@
 public class Numbers {
   private int[] numbers;
   private int size;
+  public Numbers{
+    this.numbers = numbers;
+    this.size = size;
+  }
   public static void main(String[] args){
 
   }
