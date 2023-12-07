19b18fc2170f4e94ab4c3f5ee90131cbc1604733
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2023-12-07 18:04:19 +0000

    DONE

--- projects/elementary-cellular-automaton/Eca.java
+++ projects/elementary-cellular-automaton/Eca.java
@@ -2,20 +2,21 @@ import java.util.Scanner;
 
 class Eca {
     static int ruleNum = 0;
-    static int boardLength = 15;
+    static int boardLength = 40;
+    static int rows = 30;
 
     public static void main(String[] args) {
         read();
         int[] binaryRuleNum = toBinaryArray8();
-        
+
         int[] cells = new int[boardLength];
         cells[boardLength / 2] = 1;
-        printArray(binaryRuleNum);
-        for (int i = 0; i < 10; i++) {
+        //printArray(binaryRuleNum);
+        for (int i = 0; i < rows; i++) {
 
             print(cells);
             cells = ruleTaker(binaryRuleNum, cells);
-           // ruleChecker(cells, 7 , 0, 1, 0);
+            // ruleChecker(cells, 7 , 0, 1, 0);
         }
     }
 
@@ -25,6 +26,7 @@ class Eca {
         }
         System.out.println("");
     }
+
     public static void printArray(int[] array) {
         for (int i = 0; i < array.length - 1; i++) {
             System.out.print(array[i]);
@@ -37,7 +39,6 @@ class Eca {
             return "\u25A1";
         } else if (number == 1) {
             return "\u25A0";
-            
 
         }
         // needs this in case its not 0 1
@@ -46,87 +47,98 @@ class Eca {
 
     public static int[] ruleTaker(int[] binaryRuleNum, int[] cells) {
         int[] tempCells = new int[boardLength];
-        for (int i = 0; i < boardLength; i++){
+        for (int i = 0; i < boardLength; i++) {
             // System.out.println(i);
-            if (i != 0 && i != boardLength){
-                 if (ruleChecker(cells, i, 1, 1, 1)){
+            if (i != boardLength) {
+                
+                if (ruleChecker(cells, i, 1, 1, 1)) {
                     tempCells[i] = binaryRuleNum[0];
-                    System.out.print("temp cells is ");
-                    printArray(tempCells);
-                 }
-                 if (ruleChecker(cells, i, 1, 1, 0)){
+                    //System.out.print("temp cells is ");
+                    //printArray(tempCells);
+                }
+                if (ruleChecker(cells, i, 1, 1, 0)) {
                     tempCells[i] = binaryRuleNum[1];
-                    System.out.println("temp cells" + tempCells);
-                 }
-                 if (ruleChecker(cells, i, 1, 0, 1)){
+                    //System.out.print("temp cells is ");
+                    //printArray(tempCells);
+                }
+                if (ruleChecker(cells, i, 1, 0, 1)) {
                     tempCells[i] = binaryRuleNum[2];
-                    System.out.println("temp cells" + tempCells);
-                 }
-                 if (ruleChecker(cells, i, 1, 0, 0)){
+                    //System.out.print("temp cells is ");
+                    //printArray(tempCells);
+                }
+                if (ruleChecker(cells, i, 1, 0, 0)) {
                     tempCells[i] = binaryRuleNum[3];
-                    System.out.println("temp cells" + tempCells);
-                 }
-                 if (ruleChecker(cells, i, 0, 1, 1)){
+                   //System.out.print("temp cells is ");
+                    //printArray(tempCells);
+                }
+                if (ruleChecker(cells, i, 0, 1, 1)) {
                     tempCells[i] = binaryRuleNum[4];
-                    System.out.println("temp cells" + tempCells);
-                 }
-                 if (ruleChecker(cells, i, 0, 1, 0)){
+                    //System.out.print("temp cells is ");
+                    //printArray(tempCells);
+                }
+                if (ruleChecker(cells, i, 0, 1, 0)) {
                     tempCells[i] = binaryRuleNum[5];
-                    System.out.println("temp cells" + tempCells);
-                 }
-                 if (ruleChecker(cells, i, 0, 0, 1)){
-                    System.out.println("temp cells" + tempCells);
+                    //System.out.print("this should return, temp cells is ");
+                    //printArray(tempCells);
+                }
+                if (ruleChecker(cells, i, 0, 0, 1)) {
+                    //System.out.print("temp cells is ");
+                    //printArray(tempCells);
                     tempCells[i] = binaryRuleNum[6];
-                 }
-                 if (ruleChecker(cells, i, 0, 0, 0)){
+                }
+                if (ruleChecker(cells, i, 0, 0, 0)) {
                     tempCells[i] = binaryRuleNum[7];
-                    System.out.println("temp cells" + tempCells);
-                 }
+                    //System.out.print("temp cells is ");
+                    //printArray(tempCells);
+                }
             }
         }
-        
+
         return tempCells;
     }
 
-    public static boolean ruleChecker(int[] cells, int i, int first, int second, int third){
+    public static boolean ruleChecker(int[] cells, int i, int first, int second, int third) {
         boolean rule = true;
-        
-        if (i != 0 && i != boardLength - 1){
-          rule = ((cells[i - 1] == first) && (cells[i] == second) && (cells[i + 1] == third));
-          System.out.println(rule + " index is " + i + "first is " + first + "second is " 
-          + second + " third is " + third + " cell value is " + cells[i]);
-        return rule;
-        } else if (i == 0){
-          rule = ((first == 0) && (cells[i] == second) && (cells[i + 1] == third));
-          System.out.println(rule + " index is " + i + "first is " + first + "second is " 
-          + second + " third is " + third + " cell value is " + cells[i]);
+
+        if (i != 0 && i != boardLength - 1) {
+            rule = ((cells[i - 1] == first) && (cells[i] == second) && (cells[i + 1] == third));
+            //System.out.println(rule);
+            // + " index is " + i + "first is " + first + "second is "
+                   // + second + " third is " + third + " cell value is " + cells[i]);
             return rule;
-        } else if (i == boardLength - 1){
-          rule = ((cells[i - 1] == first) && (cells[i] == second) && (third == 0));
-          System.out.println(rule + " index is " + i + "first is " + first + "second is " 
-          + second + " third is " + third + " cell value is " + cells[i]);
+        } else if (i == 0) {
+            rule = ((first == 0) && (cells[i] == second) && (cells[i + 1] == third));
+            //System.out.println("big black cocks" + rule);
+            //System.out.println(rule + " index is " + i + "first is " + first + "second is "
+               //     + second + " third is " + third + " cell value is " + cells[i]);
+            return rule;
+        } else if (i == boardLength - 1) {
+            rule = ((cells[i - 1] == first) && (cells[i] == second) && (third == 0));
+            //System.out.println(rule);
+           // System.out.println(rule + " index is " + i + "first is " + first + "second is "
+              //      + second + " third is " + third + " cell value is " + cells[i]);
             return rule;
         } else {
-          System.out.println("whoopsie Daisy somethings wrong");
+            System.out.println("whoopsie Daisy somethings wrong");
             return false;
         }
     }
-   
+
     public static int[] toBinaryArray8() {
         // orion is my source, but i edited this to fit my code
-        int[] tempBinaryArray = {0, 0, 0, 0, 0, 0, 0, 0, 0};
-            String binaryWithOutLeading0 = Integer.toBinaryString(ruleNum);
-            String stringRule =  "00000000".substring(binaryWithOutLeading0.length()) + binaryWithOutLeading0;
-            System.out.println(stringRule);
-            for (int i = 0; i < 8; i++){
-                 
-                tempBinaryArray[i] = (stringRule.charAt(i));
-                tempBinaryArray[i] = tempBinaryArray[i] % 2;
-                
-            }
-            return tempBinaryArray;
+        int[] tempBinaryArray = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        String binaryWithOutLeading0 = Integer.toBinaryString(ruleNum);
+        String stringRule = "00000000".substring(binaryWithOutLeading0.length()) + binaryWithOutLeading0;
+        System.out.println(stringRule);
+        for (int i = 0; i < 8; i++) {
+
+            tempBinaryArray[i] = (stringRule.charAt(i));
+            tempBinaryArray[i] = tempBinaryArray[i] % 2;
 
         }
+        return tempBinaryArray;
+
+    }
 
     public static void read() {
         System.out.println("");
@@ -135,4 +147,5 @@ class Eca {
         Scanner scanner = new Scanner(System.in);
         ruleNum = scanner.nextInt();
     }
+
 }
