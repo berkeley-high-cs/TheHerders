74039780147a546d757fcbc7e7a739f4b0ef5c3d
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2023-12-11 11:07:55 -0800

    Updating assessments/arrays-strings-cc/ArraysStringsCC.java

--- assessments/arrays-strings-cc/ArraysStringsCC.java
+++ assessments/arrays-strings-cc/ArraysStringsCC.java
@@ -180,6 +180,16 @@ public class ArraysStringsCC {
     }
     return total;
   }
+  public void rotateLeft(String[] array){
+    String[] newArray = new int [array.length];
+    for (int i = 0; i < array.length; i++){
+      if (i != array.length - 1){
+        newArray[i] = array[i + 1];
+      } else {
+        newArray[array.length - 1] = array[0];
+      } 
+    }
+  }
   
   
 
