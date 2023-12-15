c36cfdce7dc6674284d9a2c888a0686944838b34
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2023-12-15 09:45:45 -0800

    Updating assessments/test-question/TestQuestion.java

--- assessments/test-question/TestQuestion.java
+++ assessments/test-question/TestQuestion.java
@@ -2,7 +2,7 @@ public class TestQuestion {
   private String id;
   private String question;
   private double points;
-  public TestQuestion(String test, int qNum, String test, double pointVal){
+  public TestQuestion(String test, int qNum, String text, double pointVal){
     this.id = (test + "-" + qNum);
     this.question = question;
     this.points = points;
