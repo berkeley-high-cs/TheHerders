9d3698a16116799e4f7800c5ed87a865aa245ebc
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2023-12-15 10:02:51 -0800

    Updating assessments/test-question/TestQuestion.java

--- assessments/test-question/TestQuestion.java
+++ assessments/test-question/TestQuestion.java
@@ -26,7 +26,7 @@ public class TestQuestion {
     return points * credit;
   }
   public TestQuestion copy(String name, int qNum){
-    new TestQuestion (id, question, points);
+    TestQuestion (id, question, points);
     return newQuestion;
   }
 }
