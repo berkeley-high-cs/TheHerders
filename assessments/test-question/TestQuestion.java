59e92c22353c8e154d0101daa17848a09b82a199
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2023-12-15 09:43:41 -0800

    Updating assessments/test-question/TestQuestion.java

--- assessments/test-question/TestQuestion.java
+++ assessments/test-question/TestQuestion.java
@@ -2,4 +2,9 @@ public class TestQuestion {
   private String id;
   private String question;
   private double points;
+  public TestQuestion{
+    this.id = id;
+    this.question = question;
+    this.points = points;
+  }
 }
