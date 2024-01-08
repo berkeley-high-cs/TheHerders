78a301a4a099a611062517be110a10f6e44501d0
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2024-01-08 10:51:40 -0800

    Updating assignments/welcome-back/WelcomeBack.java

--- assignments/welcome-back/WelcomeBack.java
+++ assignments/welcome-back/WelcomeBack.java
@@ -1,5 +1,5 @@
 public class WelcomeBack{
    public static void main(String[] args){
-    System.out.println("Welcome back to CSA! tee hee");
+    System.out.println("Welcome back to CSA!");
    }
 }
