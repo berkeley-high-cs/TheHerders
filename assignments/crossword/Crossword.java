b2f19d52a3f35a0757b388d0889949a678bb1460
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2024-01-16 09:43:43 -0800

    Updating assignments/crossword/Crossword.java

--- assignments/crossword/Crossword.java
+++ assignments/crossword/Crossword.java
@@ -1,3 +1,17 @@
 public class Crossword {
-
+         public boolean fits(String guess, String word){
+          
+            if (guess.length != word.length){
+              return false;
+            }
+            for (int i = 0; i < word.length; i++){
+              if (guess.substring(Math.Min(i - 1, 0), i).equals(word.substring(Math.Min(i - 1, 0), i)) || guess.substring(Math.Min(i - 1, 0), i).equals("-")){
+                  
+              } else {
+                return false;
+              }
+            }
+            return true;
+            
+         }
 }
