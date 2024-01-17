5b1e8739ca7c8d6a03d714684cfa1c0478850872
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2024-01-17 09:42:32 -0800

    Updating assignments/crossword/Crossword.java

--- assignments/crossword/Crossword.java
+++ assignments/crossword/Crossword.java
@@ -5,11 +5,9 @@ public class Crossword {
               return false;
             }
             for (int i = 0; i < word.length(); i++){
-              if ((guess.substring(Math.max(i - 1, 0), i).equals(word.substring(Math.max(i - 1, 0), i))) || word.substring(Math.max(i - 1, 0), i).equals("-")){
+              if (!(guess.substring(Math.max(i - 1, 0), i).equals(word.substring(Math.max(i - 1, 0), i))) || word.substring(Math.max(i - 1, 0), i).equals("-")){
                   
-              } else {
-                return false;
-              }
+              } 
             }
             return true;
             
