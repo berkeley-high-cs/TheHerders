68fed2ce2dc2b25eca77bcc840561aaf3523cd70
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2024-01-17 09:53:49 -0800

    Updating assignments/crossword/Crossword.java

--- assignments/crossword/Crossword.java
+++ assignments/crossword/Crossword.java
@@ -1,22 +1,34 @@
 import java.util.*;
 
 public class Crossword {
-         public boolean fits(String guess, String word){
-          
-            if (guess.length() != word.length()){
-              return false;
-            }
-            for (int i = 0; i < word.length(); i++){
-              if ((guess.substring(Math.max(i - 1, 0), i).equals(word.substring(Math.max(i - 1, 0), i))) || word.substring(Math.max(i - 1, 0), i).equals("-")){
-                  
-              } else {
-                return false;
-              }
-            }
-            return true;
-            
-         }
-         public ArrayList<String> possiblities(String word){
-            ArrayList<String> words = new ArrayList<>();
-         }
+
+  public boolean fits(String guess, String word) {
+    if (guess.length() != word.length()) {
+      return false;
+    }
+    for (int i = 0; i < word.length(); i++) {
+      if (
+        (
+          guess
+            .substring(Math.max(i - 1, 0), i)
+            .equals(word.substring(Math.max(i - 1, 0), i))
+        ) ||
+        word.substring(Math.max(i - 1, 0), i).equals("-")
+      ) {} else {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public ArrayList<String> possiblities(String word, ArrayList<String> words) {
+    ArrayList<String> copyWords = new ArrayList<>(words);
+    for (int i = 0; i < strings.size(); i++) {
+      if (!fits(copyWords.get(i), word)) {
+        copyWords.remove(i);
+        i--;
+      }
+    }
+    return copyWords;
+  }
 }
