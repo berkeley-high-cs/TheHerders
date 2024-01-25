b20f836bf1101a5ecd33e77766ae26c06c42c271
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2024-01-25 09:46:43 -0800

    Updating assignments/primes-2/Primes.java

--- assignments/primes-2/Primes.java
+++ assignments/primes-2/Primes.java
@@ -27,6 +27,9 @@ public class Primes {
     return list;
   }
   public boolean isPrime(int n){
+    if (n < 2){
+      return false;
+    }
     for (int i = 2; i < Math.sqrt(n); i++){
       if (n % i == 0){
         return false;
