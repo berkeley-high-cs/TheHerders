7b058d237b475e8c47515fa373215219f80afa6f
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2024-01-26 09:45:10 -0800

    Creating assignments/is-sorted/IsSorted.java

---
+++ assignments/is-sorted/IsSorted.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class IsSorted {
+  public boolean isSorted(ArrayList<Integer> list){
+    int highestNum = 0;
+    for (int i = 0; i < list.size(); i++){
+      if (list[i] < highest num){
+        return false;
+      } else {
+        highestNum = list[i];
+      }
+    }
+  }
+
+}
