2ba07240d7c0ea8930afb7d93fcf0cdd24452726
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2024-01-26 09:45:27 -0800

    Updating assignments/is-sorted/IsSorted.java

--- assignments/is-sorted/IsSorted.java
+++ assignments/is-sorted/IsSorted.java
@@ -4,7 +4,7 @@ public class IsSorted {
   public boolean isSorted(ArrayList<Integer> list){
     int highestNum = 0;
     for (int i = 0; i < list.size(); i++){
-      if (list[i] < highest num){
+      if (list[i] < highestNum){
         return false;
       } else {
         highestNum = list[i];
