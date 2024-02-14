11924a0fc042304f984a23d6d0c631c94864444f
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2024-02-14 09:38:03 -0800

    Updating assignments/string-search/StringSearch.java

--- assignments/string-search/StringSearch.java
+++ assignments/string-search/StringSearch.java
@@ -1,5 +1,13 @@
 import java.util.ArrayList;
 
 public class StringSearch {
-
+  public String longestString(ArrayList<String> list){
+    int longestIndex
+    for (int i = 0; i < list.size(); i++){
+      if (list.get(i).length() > longestIndex){
+        longestIndex = i;
+      }
+    }
+    return list.get(longestIndex);
+  }
 }
