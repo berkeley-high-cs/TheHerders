4c186bb9b997db3f636fd055c374217f0ef07c6c
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2024-02-14 10:27:39 -0800

    Updating assignments/string-search/StringSearch.java

--- assignments/string-search/StringSearch.java
+++ assignments/string-search/StringSearch.java
@@ -27,8 +27,8 @@ public class StringSearch {
     
     for (int i = 0; i < word.length(); i++){
       
-      if (word.substring(i, i + 1).equals(consecutiveList.get(Math.min(i,consecutiveList.size()-1),))){
-        consecutiveList.set(Math.min(i,consecutiveList.size()-1),consecutiveList.get(Math.min(i,consecutiveList.size()-1),) + word.substring(i,i+1));
+      if (word.substring(i, i + 1).equals(consecutiveList.get(Math.min(i,consecutiveList.size()-1)))){
+        consecutiveList.set(Math.min(i,consecutiveList.size()-1),consecutiveList.get(Math.min(i,consecutiveList.size()-1)) + word.substring(i,i+1));
       }else {
         consecutiveList.add(word.substring(i,i+1));
       }
