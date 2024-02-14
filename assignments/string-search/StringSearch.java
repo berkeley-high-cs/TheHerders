e200a5c78fdecf582b42583595f09d1979d0b27e
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2024-02-14 09:59:36 -0800

    Updating assignments/string-search/StringSearch.java

--- assignments/string-search/StringSearch.java
+++ assignments/string-search/StringSearch.java
@@ -23,7 +23,7 @@ public class StringSearch {
     String substring = "";
     int longestSubstring = 0;
     for (int i = 0; i < word.length(); i++){
-      if (word.substring(i, i+1).equals(substring.substring(Math.min(substring.length, i+1),Math.max(i+2, substring.length()) ))){
+      if (word.substring(i, i+1).equals(substring.substring(Math.min(substring.length(), i+1),Math.max(i+2, substring.length()) ))){
         substring = substring + word.substring(i,i+1);
       }
     }
