e55a38edf0b87d706d78a37f9b195a34397f5e4e
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2024-02-15 09:59:20 -0800

    Updating assignments/game-algorithms/GameAlgorithms.java

--- assignments/game-algorithms/GameAlgorithms.java
+++ assignments/game-algorithms/GameAlgorithms.java
@@ -10,24 +10,31 @@ public class GameAlgorithms {
   }
   public int sum4Neighbors(int[][] grid, int x, int y){
     int total = 0;
-
-    if (inBounds(grid, x+1, y)){
-      total += grid[x+1][y];
+    int[] xValues = {1,-1,0,0};
+    int[] yValues = {0,0,1,-1}
+    for (int i = 0; i < 4; i++){
+      if (inBounds(grid, x+xValues[i], y+yValues[i])){
+      total += grid[x++xValues[i]][y+yValues[i]];
     } 
-    if (inBounds(grid, x-1, y)){
-      total += grid[x-1][y];
     }
-     if (inBounds(grid, x, y+1)){
-      total += grid[x][y+1];
-    } 
-    if (inBounds(grid, x, y-1)){
-      total += grid[x][y-1];
-    }
+    // if (inBounds(grid, x+1, y)){
+    //   total += grid[x+1][y];
+    // } 
+    // if (inBounds(grid, x-1, y)){
+    //   total += grid[x-1][y];
+    // }
+    //  if (inBounds(grid, x, y+1)){
+    //   total += grid[x][y+1];
+    // } 
+    // if (inBounds(grid, x, y-1)){
+    //   total += grid[x][y-1];
+    // }
     return total;
 
   }
   public int sum8Neighbors(int[][] grid, int x, int y){
     int total = 0;
+    
 
     if (inBounds(grid, x+1, y)){
       total += grid[x+1][y];
