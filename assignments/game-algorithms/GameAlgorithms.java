91c83652e8228c147910712f57efd094c89aefcd
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2024-02-15 09:43:37 -0800

    Updating assignments/game-algorithms/GameAlgorithms.java

--- assignments/game-algorithms/GameAlgorithms.java
+++ assignments/game-algorithms/GameAlgorithms.java
@@ -8,5 +8,13 @@ public class GameAlgorithms {
     } 
     return true;
   }
+  public int sum4Neighbors(int[][] grid, int x, int y){
+    int maxX = Math.max(x - 1, 0);
+    int minX = Math.min(x + 1, grid.length);
+    int maxY = Math.max(y - 1,0);
+    int minY = Math.min(y + 1, grid[x].length);
+    return grid[maxX][y] + grid[x][maxY] + grid[mixX][y] + grid[x][minY];
+
+  }
 
 }
