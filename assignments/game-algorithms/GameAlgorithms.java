bf5ea64a535cafdd88e9d13f09c0a9f84133dee7
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2024-02-15 09:39:09 -0800

    Updating assignments/game-algorithms/GameAlgorithms.java

--- assignments/game-algorithms/GameAlgorithms.java
+++ assignments/game-algorithms/GameAlgorithms.java
@@ -1,9 +1,9 @@
 public class GameAlgorithms {
   public boolean inBounds(int[][] bounds, int x, int y){
-    if (x >= bounds.length){
+    if (x >= bounds.length || x < 0){
       return false;
     } 
-    if (y >= bounds[x].length){
+    if (y >= bounds[x].length || y < 0){
       return false;
     } 
     return true;
