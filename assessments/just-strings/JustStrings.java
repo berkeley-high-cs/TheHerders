4d6a1caa85ef90bbb717daa1ae83417e07978d68
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2024-02-21 10:08:34 -0800

    Updating assessments/just-strings/JustStrings.java

--- assessments/just-strings/JustStrings.java
+++ assessments/just-strings/JustStrings.java
@@ -39,11 +39,15 @@ public class JustStrings {
   public String ubbieDubbie(String word){
     String newWord = word;
     for (int i = 0; i < word.length(); i++){
-      if (isVowel(word.substring(i, i+1)) && !isVowel(word.substring(i-1, i))){
+      if (isVowel(word.substring(i, i+1)) && i != 0 || !isVowel(word.substring(i-1, i))){
         newWord = newWord.substring(0, i) + "ub" + newWord.substring(i);
       }
     }
     return newWord;
+  }
+  public String runLengthEncoding(String word){
+    String newWord = word;
+    for (int i)
   }
 
 }
