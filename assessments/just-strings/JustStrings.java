f92b843f56ed1aaa78ea2ab58dbf7f1c5888c766
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2024-02-21 09:42:58 -0800

    Updating assessments/just-strings/JustStrings.java

--- assessments/just-strings/JustStrings.java
+++ assessments/just-strings/JustStrings.java
@@ -6,7 +6,7 @@ public class JustStrings {
     return "aeiou".indexOf(c) != -1;
   }
   public String insert(String word1, String word2, int index){
-    String newWord = word1.substring(0, index) + word2 + word1.substring(index);
-    return newWord;
+    return word1.substring(0, index) + word2 + word1.substring(index);
+    
   }
 }
