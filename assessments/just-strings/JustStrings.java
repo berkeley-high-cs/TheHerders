4e12396dd261b4cfc5462d9d9a87cc53f81e0bcc
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2024-02-21 09:47:15 -0800

    Updating assessments/just-strings/JustStrings.java

--- assessments/just-strings/JustStrings.java
+++ assessments/just-strings/JustStrings.java
@@ -9,6 +9,10 @@ public class JustStrings {
     return word1.substring(0, index) + word2 + word1.substring(index);
   }
   public String deleteFirst(String word1, String word2){
-    return word1.substring(0, word1.indexOf(word2)) + word1.substring(word1.indexOf(word2), word1.indexOf(word2) + word2.length());
+    String newWord = word1.substring(0, word1.indexOf(word2));
+    newWord = newWord + word1.substring(word1.indexOf(word2) + word2.length());
+    //return word1.substring(0, word1.indexOf(word2)) + word1.substring(word1.indexOf(word2), word1.indexOf(word2) + word2.length());
+    return newWord;
   }
+
 }
