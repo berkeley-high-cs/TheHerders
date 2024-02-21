188f52e584ed70b3ece76843ea8c205992a86ed9
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2024-02-21 10:30:45 -0800

    Updating assessments/just-strings/JustStrings.java

--- assessments/just-strings/JustStrings.java
+++ assessments/just-strings/JustStrings.java
@@ -54,13 +54,10 @@ public class JustStrings {
     int ubAmount = 0;
     for (int i = 0; i < word.length(); i++) {
       if (isVowel(word.substring(i, i + 1))) {
-        if (i == 0) {
+
           newWord = newWord.substring(0, i + 2 * ubAmount) + "ub" + newWord.substring(i +  2 * ubAmount);
           ubAmount++;
-        } else if (!isVowel(word.substring(i - 1, i))) {
-          newWord = newWord.substring(0, i +  2 * ubAmount) + "ub" + newWord.substring(i + 2 * ubAmount);
-          ubAmount++;
-        }
+        
       }
     }
     return newWord;
