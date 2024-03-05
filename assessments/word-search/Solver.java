f5f238d5ea631a74f67d4cd9327c9f9653faa1fe
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2024-03-05 09:49:34 -0800

    Updating assessments/word-search/Solver.java

--- assessments/word-search/Solver.java
+++ assessments/word-search/Solver.java
@@ -8,7 +8,7 @@ public class Solver {
   int[] xAdds = {-1, -1, -1, 0, 0, 0, 1, 1, 1};
   int[] yAdds = {-1, 0, 1, -1, 0, 1, -1, 0, 1};
   for (int x = 0; x < puzzle.length; x++){
-    for (int y = 0; y < puzzle[i].length; y++){
+    for (int y = 0; y < puzzle[x].length; y++){
       if (puzzle[x][y].equals(word.substring(0,1))){
           for (int i = 0; i < 8; i++){
             if (inBounds(x + xAdds[i], y + yAdds[i]) && word.substring(1,2).equals(puzzle[x+xAdds[i]][y+yAdds[i]])){
