8904fe8ad20347f1fe065bc7c2f9cfb932f24047
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2024-03-05 09:57:22 -0800

    Updating assessments/word-search/Solver.java

--- assessments/word-search/Solver.java
+++ assessments/word-search/Solver.java
@@ -10,7 +10,7 @@ public class Solver {
     for (int y = 0; y < puzzle[x].length; y++){
       if (puzzle[x][y].equals(word.substring(0,1))){
           for (int i = 0; i < 8; i++){
-            if (checkAround(x, y, puzzle, word)){
+            if (checkAround(x, y, i, puzzle, word)){
               return true; //so far, just checks for first two characters of a string
             }
           }
@@ -19,7 +19,7 @@ public class Solver {
   }
   return false;
  }
- public boolean checkAround(int x, int y, String[][] puzzle, String word){
+ public boolean checkAround(int x, int y, int i, String[][] puzzle, String word){
    int[] xAdds = {-1, -1, -1, 0, 0, 0, 1, 1, 1};
   int[] yAdds = {-1, 0, 1, -1, 0, 1, -1, 0, 1};
   return inBounds(x + xAdds[i], y + yAdds[i], puzzle) && word.substring(1,2).equals(puzzle[x+xAdds[i]][y+yAdds[i]]);
