47a5e5ff149aa4c6ffe1a64041b5720c6ae1d82f
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2024-03-05 10:12:11 -0800

    Updating assessments/word-search/Solver.java

--- assessments/word-search/Solver.java
+++ assessments/word-search/Solver.java
@@ -13,13 +13,13 @@ public class Solver {
 
         if (puzzle[x][y].equals(word.substring(0, 1))) { //check if its equal to the first charcter in word
 
-          for (int i = 0; i < 8; i++) { //if it is, loop 8 times checking around that place
+          for (int i = 0; i < 8; i++) { //if it is, loop 8 times checking around that place 
 
             if (checkAround(x, y, i, puzzle, word)) { // if it finds the next character, 
 
-              for (int z = 0; z < word.length() - 2; z++) { // it loops through the word count minus the words weve checked
+              for (int z = 1; z < word.length(); z++) { // it loops through the word count minus the words weve checked
 
-                if (checkAround(x, y, i, puzzle, word)) { //and goes as far as it can
+                if (checkAround(x, y, z, i, puzzle, word)) { //and goes as far as it can
                   lettersMatched++; // adding for each letter found
                 }
                 // return true; //so far, just checks for first two characters of a string
@@ -41,6 +41,7 @@ public class Solver {
   public boolean checkAround(
     int x,
     int y,
+    int z,
     int i,
     String[][] puzzle,
     String word
@@ -48,8 +49,8 @@ public class Solver {
     int[] xAdds = { -1, -1, -1, 0, 0, 0, 1, 1, 1 };
     int[] yAdds = { -1, 0, 1, -1, 0, 1, -1, 0, 1 };
     return (
-      inBounds(x + xAdds[i], y + yAdds[i], puzzle) &&
-      word.substring(1, 2).equals(puzzle[x + xAdds[i]][y + yAdds[i]])
+      inBounds(x + xAdds[i] * z, y + yAdds[i] * z, puzzle) &&
+      word.substring(z, z+1).equals(puzzle[x + xAdds[i]][y + yAdds[i]])
     );
   }
 }
