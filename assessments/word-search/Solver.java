abd625fe3f4d1b366c171033ebeebecc697fe12e
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2024-03-05 09:49:23 -0800

    Updating assessments/word-search/Solver.java

--- assessments/word-search/Solver.java
+++ assessments/word-search/Solver.java
@@ -4,4 +4,19 @@ public class Solver {
  public boolean inBounds(int x, int y, String[][] wall){
     return (x < wall.length && y < wall[x].length && x >= 0 && y >= 0);
  }
+ public boolean inGrid(String word, String[][] puzzle){
+  int[] xAdds = {-1, -1, -1, 0, 0, 0, 1, 1, 1};
+  int[] yAdds = {-1, 0, 1, -1, 0, 1, -1, 0, 1};
+  for (int x = 0; x < puzzle.length; x++){
+    for (int y = 0; y < puzzle[i].length; y++){
+      if (puzzle[x][y].equals(word.substring(0,1))){
+          for (int i = 0; i < 8; i++){
+            if (inBounds(x + xAdds[i], y + yAdds[i]) && word.substring(1,2).equals(puzzle[x+xAdds[i]][y+yAdds[i]])){
+              return true; //so far, just checks for first two characters of a string
+            }
+          }
+      }
+    }
+  }
+ }
 }
