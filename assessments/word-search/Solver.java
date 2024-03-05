c2fcd0dc22aa2a98c76df051e63ec9738d0cd7cc
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2024-03-05 09:37:37 -0800

    Updating assessments/word-search/Solver.java

--- assessments/word-search/Solver.java
+++ assessments/word-search/Solver.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class Solver {
- public boolean inBounds(int x, int y, Strings[][] wall){
+ public boolean inBounds(int x, int y, String[][] wall){
     return (x < wall.length() && y < wall[x].length());
  }
 }
