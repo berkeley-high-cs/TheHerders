b1b887bc8d809ed08b5189792b4588fde2729133
StevenDen <119467873+GrandeGringo@users.noreply.github.com>
2024-03-05 10:01:55 -0800

    Updating assessments/word-search/Solver.java

--- assessments/word-search/Solver.java
+++ assessments/word-search/Solver.java
@@ -1,27 +1,43 @@
 import java.util.*;
 
 public class Solver {
- public boolean inBounds(int x, int y, String[][] wall){
-    return ( (x >= 0 && y >= 0) && (x < wall.length && y < wall[x].length));
- }
- public boolean inGrid(String word, String[][] puzzle){
- 
-  for (int x = 0; x < puzzle.length; x++){
-    for (int y = 0; y < puzzle[x].length; y++){
-      if (puzzle[x][y].equals(word.substring(0,1))){
-          for (int i = 0; i < 8; i++){
-            if (checkAround(x, y, i, puzzle, word)){
-              return true; //so far, just checks for first two characters of a string
+
+  public boolean inBounds(int x, int y, String[][] wall) {
+    return ((x >= 0 && y >= 0) && (x < wall.length && y < wall[x].length));
+  }
+
+  public boolean inGrid(String word, String[][] puzzle) {
+    for (int x = 0; x < puzzle.length; x++) {
+      for (int y = 0; y < puzzle[x].length; y++) {
+        if (puzzle[x][y].equals(word.substring(0, 1))) {
+          for (int i = 0; i < 8; i++) {
+            if (checkAround(x, y, i, puzzle, word)) {
+              for (int z = 0; z < word.length; z++) {
+                if (!checkAround(x, y, i, puzzle, word)) {
+                  return false;
+                }
+                // return true; //so far, just checks for first two characters of a string
+              }
             }
           }
+        }
       }
     }
+    return true;
   }
-  return false;
- }
- public boolean checkAround(int x, int y, int i, String[][] puzzle, String word){
-   int[] xAdds = {-1, -1, -1, 0, 0, 0, 1, 1, 1};
-  int[] yAdds = {-1, 0, 1, -1, 0, 1, -1, 0, 1};
-  return inBounds(x + xAdds[i], y + yAdds[i], puzzle) && word.substring(1,2).equals(puzzle[x+xAdds[i]][y+yAdds[i]]);
- }
+
+  public boolean checkAround(
+    int x,
+    int y,
+    int i,
+    String[][] puzzle,
+    String word
+  ) {
+    int[] xAdds = { -1, -1, -1, 0, 0, 0, 1, 1, 1 };
+    int[] yAdds = { -1, 0, 1, -1, 0, 1, -1, 0, 1 };
+    return (
+      inBounds(x + xAdds[i], y + yAdds[i], puzzle) &&
+      word.substring(1, 2).equals(puzzle[x + xAdds[i]][y + yAdds[i]])
+    );
+  }
 }
