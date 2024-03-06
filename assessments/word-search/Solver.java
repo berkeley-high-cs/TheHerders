44b4d87f8ae7c253d82792ca35358114c69d28ed
StevenDen <119467873+TheHerders@users.noreply.github.com>
2024-03-06 09:59:06 -0800

    Updating assessments/word-search/Solver.java

--- assessments/word-search/Solver.java
+++ assessments/word-search/Solver.java
@@ -53,9 +53,9 @@ public class Solver {
     int[] yAdds = { -1, 0, 1, -1, 0, 1, -1, 0, 1 };
     int xCoor = x + (xAdds[i] * z);
     int yCoor = y + (yAdds[i] * z); 
-    System.err.println("XCoor: " + xCoor + " yCoor: " + yCoor + " word.substring(z,z + 1): " + word.substring(z,z + 1));
+    //System.err.println("XCoor: " + xCoor + " yCoor: " + yCoor + " word.substring(z,z + 1): " + word.substring(z,z + 1));
     if (inBounds(xCoor,yCoor, puzzle)){
-      System.err.println("puzzle[xCoor][yCoor]: " + puzzle[xCoor][yCoor] );
+      //System.err.println("puzzle[xCoor][yCoor]: " + puzzle[xCoor][yCoor] );
     }
 
     
