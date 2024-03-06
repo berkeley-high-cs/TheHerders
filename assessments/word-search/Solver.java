8eb99adc71e08c3fd12197fa368173551d063d0f
StevenDen <119467873+TheHerders@users.noreply.github.com>
2024-03-06 10:37:08 -0800

    Updating assessments/word-search/Solver.java

--- assessments/word-search/Solver.java
+++ assessments/word-search/Solver.java
@@ -9,39 +9,41 @@ public class Solver {
   public boolean inGrid(String word, String[][] puzzle) {
     int lettersMatched = 0;
     int direction = 0;
-    
+
     for (int x = 0; x < puzzle.length; x++) {
       for (int y = 0; y < puzzle[x].length; y++) { //iterate through every character in the 2d array
 
         if (puzzle[x][y].equals(word.substring(0, 1))) {
-          
+
           lettersMatched++; //check if its equal to the first charcter in word
-                for (int i = 0; i < 7; i++){
-                  
-                     if (checkAround(x, y, 1, i, puzzle, word)){
-                    direction = i;
-                    i = 8;
-                    lettersMatched++;
-                    }
-                }
+
+          for (int i = 0; i < 7; i++) {
+
+            if (checkAround(x, y, 1, i, puzzle, word)) {
+
+              direction = i;
+              i = 8;
+              lettersMatched++;
+
               for (int z = 2; z < word.length(); z++) { //it then loops through checking if the second letter is around it a bunch
-                  
-                  
+
                 if ((checkAround(x, y, z, direction, puzzle, word))) { //and goes as far as it can
+                
                   lettersMatched++; // adding for each letter found
                 }
-              } 
-              if (lettersMatched == word.length()){ // if the letters found equal the word length
-                return true; //we did it
-              } else {
-                lettersMatched = 0; //else we try again
               }
-            
-          
+            }
+          }
+
+          if (lettersMatched == word.length()) { // if the letters found equal the word length
+            return true; //we did it
+          } else {
+            lettersMatched = 0; //else we try again
+          }
         }
       }
     }
-   
+
     return false;
   }
 
@@ -56,8 +58,8 @@ public class Solver {
     int[] xAdds = { -1, -1, -1, 0, 0, 1, 1, 1 };
     int[] yAdds = { -1, 0, 1, -1, 1, -1, 0, 1 };
     int xCoor = x + (xAdds[i] * z);
-    int yCoor = y + (yAdds[i] * z); 
-   
+    int yCoor = y + (yAdds[i] * z);
+
     // System.err.println("XCoor: " + xCoor + " yCoor: " + yCoor + " word.substring(z,z + 1): " + word.substring(z,z + 1));
     // if (inBounds(xCoor,yCoor, puzzle)){
     //   System.err.println("puzzle[xCoor][yCoor]: " + puzzle[xCoor][yCoor] );
@@ -66,19 +68,16 @@ public class Solver {
     //   System.err.println("direction: " + i);
     // }
 
-    
-  
-      return (inBounds(xCoor,yCoor, puzzle) && (word.substring(z,z + 1).equals(puzzle[xCoor][yCoor])));
-
-
-
-   
+    return (
+      inBounds(xCoor, yCoor, puzzle) &&
+      (word.substring(z, z + 1).equals(puzzle[xCoor][yCoor]))
+    );
   }
 
-  public ArrayList<String> found(String[][] wall, ArrayList<String> list){
+  public ArrayList<String> found(String[][] wall, ArrayList<String> list) {
     ArrayList<String> newList = new ArrayList<>();
-    for(int i = 0; i < list.size(); i++){
-      if (inGrid(list.get(i), wall)){
+    for (int i = 0; i < list.size(); i++) {
+      if (inGrid(list.get(i), wall)) {
         newList.add(list.get(i));
       }
     }
@@ -86,6 +85,6 @@ public class Solver {
   }
   // public boolean checkPlace(int x, int y, String letter, String[][] puzzle){
   //   return inBounds(x, y, puzzle) && (letter.equals(puzzle[x][y]));
-   
+
   // }
 }
