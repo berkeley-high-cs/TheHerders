1dbcf4aee3e052bfe8fcacdc45bad1625c153d24
StevenDen <119467873+TheHerders@users.noreply.github.com>
2024-03-07 09:41:42 -0800

    Updating assessments/word-search-4/Solver.java

--- assessments/word-search-4/Solver.java
+++ assessments/word-search-4/Solver.java
@@ -2,4 +2,33 @@ import java.util.*;
 
 public class Solver {
 
+  public boolean inBounds(int row, int col, String[][] wall) {
+    return (
+      (row >= 0 && col >= 0) && (row < wall.length && col < wall[row].length)
+    );
+  }
+
+  public boolean startingAt(String word, String[][] puzzle, int row, int col) {
+    int[] xAdds = { -1, -1, -1, 0, 0, 1, 1, 1 };
+    int[] yAdds = { -1, 0, 1, -1, 1, -1, 0, 1 };
+    for (int i = 0; i < 8; i++) {
+      if (atAndInDirection(word, puzzle, row, col, xAdds[i], yAdds[i])) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public boolean inGrid(String word, String[][] puzzle) {
+    for (int row = 0; row < puzzle.length; row++) {
+      for (int col = 0; col < puzzle[row].length; col++) { //iterate through evercol character in the 2d arracol
+        if (puzzle[row][col].equals(word.substring(0, 1))) {
+          if (startingAt(word, puzzle, row, col)) {
+            return true;
+          }
+        }
+      }
+    }
+    return false;
+  }
 }
