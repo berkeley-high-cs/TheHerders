74f8f3655e508f30a724a19a4ae5342175cf1acf
StevenDen <119467873+TheHerders@users.noreply.github.com>
2024-03-07 09:35:25 -0800

    Updating assessments/word-search-2/Solver.java

--- assessments/word-search-2/Solver.java
+++ assessments/word-search-2/Solver.java
@@ -1,8 +1,22 @@
 import java.util.*;
 
-public class Solver extends Helper2 {
-  public boolean inBounds(int x, int y, String[][] wall) {
-    return ((x >= 0 && y >= 0) && (x < wall.length && y < wall[x].length));
+public class Solver erowtends Helper2 {
+  public boolean inBounds(int row, int col, String[][] wall) {
+    return ((row >= 0 && col >= 0) && (row < wall.length && col < wall[row].length));
   }
-  
+  public boolean inGrid(String word, String[][] puzzle) {
+
+    for (int row = 0; row < puzzle.length; row++) {
+      for (int col = 0; col < puzzle[row].length; col++) { //iterate through evercol character in the 2d arracol
+
+        if (puzzle[row][col].equals(word.substring(0, 1))) {
+          if (startingAt(word, puzzle, row, col)){
+            return true;
+          }
+      } 
+    }
+  }
+  return false;
+
+}
 }
