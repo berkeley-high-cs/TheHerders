e7cb6d41f1bb756fd4b906ea331a918e69798a32
StevenDen <119467873+TheHerders@users.noreply.github.com>
2024-03-07 09:41:06 -0800

    Updating assessments/word-search-3/Solver.java

--- assessments/word-search-3/Solver.java
+++ assessments/word-search-3/Solver.java
@@ -14,4 +14,19 @@ public class Solver extends Helper3 {
     }
     return false;
   }
+   public boolean inGrid(String word, String[][] puzzle) {
+
+    for (int row = 0; row < puzzle.length; row++) {
+      for (int col = 0; col < puzzle[row].length; col++) { //iterate through evercol character in the 2d arracol
+
+        if (puzzle[row][col].equals(word.substring(0, 1))) {
+          if (startingAt(word, puzzle, row, col)){
+            return true;
+          }
+      } 
+    }
+  }
+  return false;
+
+}
 }
