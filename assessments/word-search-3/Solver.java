8175d85ceeb5b15289651b29f44dc1678e3ced31
StevenDen <119467873+TheHerders@users.noreply.github.com>
2024-03-07 09:40:45 -0800

    Updating assessments/word-search-3/Solver.java

--- assessments/word-search-3/Solver.java
+++ assessments/word-search-3/Solver.java
@@ -1,5 +1,17 @@
 import java.util.*;
 
 public class Solver extends Helper3 {
-
+   public boolean inBounds(int row, int col, String[][] wall) {
+    return ((row >= 0 && col >= 0) && (row < wall.length && col < wall[row].length));
+  }
+  public boolean startingAt(String word, String[][] puzzle, int row, int col){
+    int[] xAdds = { -1, -1, -1, 0, 0, 1, 1, 1 };
+    int[] yAdds = { -1, 0, 1, -1, 1, -1, 0, 1 };
+    for (int i = 0; i < 8; i++){
+      if (atAndInDirection(word, puzzle, row, col, xAdds[i], yAdds[i])){
+        return true;
+      }
+    }
+    return false;
+  }
 }
