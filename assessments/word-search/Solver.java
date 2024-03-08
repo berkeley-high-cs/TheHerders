181b1342dc42900e8172522e345fbbdd4113d3d7
StevenDen <119467873+TheHerders@users.noreply.github.com>
2024-03-07 18:47:47 -0800

    Updating assessments/word-search/Solver.java

--- assessments/word-search/Solver.java
+++ assessments/word-search/Solver.java
@@ -3,9 +3,7 @@ import java.util.*;
 public class Solver {
 
   public boolean inBounds(int row, int col, String[][] wall) {
-    return (
-      (row >= 0 && col >= 0) && (row < wall.length && col < wall[row].length)
-    );
+    return ((row >= 0 && col >= 0) && (row < wall.length && col < wall[row].length));
   }
 
   public boolean inGrid(String word, String[][] puzzle) {
@@ -16,29 +14,40 @@ public class Solver {
 
     for (int row = 0; row < puzzle.length; row++) {
       for (int col = 0; col < puzzle[row].length; col++) { //iterate through every character in the 2d array
+
         if (puzzle[row][col].equals(word.substring(0, 1))) {
+
           lettersMatched++; //check if its equal to the first charcter in word
 
           for (int i = 0; i < 8; i++) {
+
             if (checkAround(row, col, 1, i, puzzle, word)) {
+
               direction = i;
               lettersMatched++;
-              if (secondLetterFound) {
-                for (int z = 2; z < word.length(); z++) { //it then loops through checking if the second letter is around it a bunch
-                  if ((checkAround(row, col, z, direction, puzzle, word))) { //and goes as far as it can
-                    errorTestingCheckAround = true;
-                    lettersMatched++; // adding for each letter found
-                  } else {
-                    errorTestingCheckAround = false;
-                  }
+              for (int z = 2; z < word.length(); z++) { //it then loops through checking if the second letter is around it a bunch
+                if ((checkAround(row, col, z, direction, puzzle, word))) { //and goes as far as it can
+                  errorTestingCheckAround = true;
+                  lettersMatched++; // adding for each letter found
+                } else {
+                  errorTestingCheckAround = false;
                 }
               }
-            }
+              
+            } 
+            
+            
+          
+            
           }
+          
+             
+
 
           if (lettersMatched == word.length()) { // if the letters found equal the word length
             return true; //we did it
           } else {
+            
             lettersMatched = 0; //else we try again
           }
         }
@@ -60,13 +69,14 @@ public class Solver {
     int[] colAdds = { -1, 0, 1, -1, 1, -1, 0, 1 };
     int rowCoor = row + (rowAdds[i] * z);
     int colCoor = col + (colAdds[i] * z);
+    
+      //System.err.println("direction: " + i);
 
-    //System.err.println("direction: " + i);
+    
 
-    return (
-      inBounds(rowCoor, colCoor, puzzle) &&
-      (word.substring(z, z + 1).equals(puzzle[rowCoor][colCoor]))
-    );
+   
+
+    return (inBounds(rowCoor, colCoor, puzzle) && (word.substring(z, z + 1).equals(puzzle[rowCoor][colCoor])));
   }
 
   public ArrayList<String> found(String[][] wall, ArrayList<String> list) {
@@ -79,17 +89,19 @@ public class Solver {
     return newList;
   }
 }
+
 // System.err.println("////////////////////////////////////////////////////////////////////////////");
 //  System.err.println("////////////////////////////////////////////////////////////////////////////");
 //  System.err.println(" word: " + word);
 //  System.err.println("puzzle[rowCoor][row]: " + puzzle[row][col] );
-// System.err.println("letters Matched: " + lettersMatched);
+ // System.err.println("letters Matched: " + lettersMatched);
 //  System.err.println("direction: " + direction);
 // System.err.println("errorTestingCheckAround" + errorTestingCheckAround);
 //        System.err.println("////////////////////////////////////////////////////////////////////////////");
 //  System.err.println("////////////////////////////////////////////////////////////////////////////");
-//   System.err.println("word: " + word);
+//   System.err.println("word: " + word);       
 //   System.err.println("rowCoor: " + rowCoor + " colCoor: " + colCoor + " word.substring(z,z + 1): " + word.substring(z,z + 1));
 // if (inBounds(rowCoor,colCoor, puzzle)){
 //   System.err.println("puzzle[rowCoor][colCoor]: " + puzzle[rowCoor][colCoor] );
 // }
+   
