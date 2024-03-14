5f4a25d01b4f2839fcbe800076455f9d801863ee
StevenDen <119467873+TheHerders@users.noreply.github.com>
2024-03-14 09:41:06 -0700

    Updating assessments/strings-3/Strings3.java

--- assessments/strings-3/Strings3.java
+++ assessments/strings-3/Strings3.java
@@ -4,4 +4,7 @@ public class Strings3 {
   public String insertBefore(String word, String insert, String index){
     return word.substring(0, word.indexOf(index)) + insert + word.substring(word.indexOf(index));
   }
+  public String insertAfter(String word, String insert, String index){
+    return word.substring(0, word.indexOf(index) + index.length()) + insert + word.substring(word.indexOf(index) + index.length());
+  }
 }
