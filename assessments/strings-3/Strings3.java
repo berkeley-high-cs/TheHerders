3abeebc4bb9878633a244982e1581ce1fe91ac1c
StevenDen <119467873+TheHerders@users.noreply.github.com>
2024-03-14 09:43:54 -0700

    Updating assessments/strings-3/Strings3.java

--- assessments/strings-3/Strings3.java
+++ assessments/strings-3/Strings3.java
@@ -7,4 +7,11 @@ public class Strings3 {
   public String insertAfter(String word, String insert, String index){
     return word.substring(0, word.indexOf(index) + index.length()) + insert + word.substring(word.indexOf(index) + index.length());
   }
+  public String reverse(String word){
+    String reversed = "";
+    for (int i = word.length(); i < 0; i--){
+      reversed = reversed + word.substring(i, i - 1);
+    }
+    return reversed;
+  }
 }
