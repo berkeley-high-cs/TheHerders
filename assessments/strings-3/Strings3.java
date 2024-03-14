f99396241dda64caffa00ef6d2ef7bb8058b6146
StevenDen <119467873+TheHerders@users.noreply.github.com>
2024-03-14 10:05:43 -0700

    Updating assessments/strings-3/Strings3.java

--- assessments/strings-3/Strings3.java
+++ assessments/strings-3/Strings3.java
@@ -64,7 +64,7 @@ public class Strings3 {
   public ArrayList<Integer> numbers(String word){
      ArrayList<Integer> list = new ArrayList<>();
     String num = "69";
-    System.err.println(num.toInt() + 2);
+    System.err.println(num.toInteger() + 2);
     return list;
   }
 }
