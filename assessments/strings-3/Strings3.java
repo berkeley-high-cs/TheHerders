69eeac714bf9edd6e0ecee0eed8cd6e7c4d60e7c
StevenDen <119467873+TheHerders@users.noreply.github.com>
2024-03-14 09:39:33 -0700

    Updating assessments/strings-3/Strings3.java

--- assessments/strings-3/Strings3.java
+++ assessments/strings-3/Strings3.java
@@ -1,5 +1,9 @@
 import java.util.ArrayList;
 
 public class Strings3 {
-
+  public String insertBefore(String word, String insert, String index){
+    String newWord = word;
+    newWord = newWord.substring(0, word.indexOf(index)) + insert + newWord.substring(word.indexOf(index));
+    return newWord;
+  }
 }
