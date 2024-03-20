2dc7be578487624b7399804b8255e0193f8ee1c9
StevenDen <119467873+TheHerders@users.noreply.github.com>
2024-03-20 10:01:37 -0700

    Updating assignments/string-recursion/StringRecursion.java

--- assignments/string-recursion/StringRecursion.java
+++ assignments/string-recursion/StringRecursion.java
@@ -10,7 +10,7 @@ public class StringRecursion {
   public int countXs(String word){
     if (word.isEmpty()){
       return 0;
-    } else if(word.contains("x")){
+    } else if(!word.contains("x")){
       return 1;
     } else {
       return countXs(word.substring(1)) + 1;
