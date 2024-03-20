557c9663748d09200a645668d29a3ce7613de6f6
StevenDen <119467873+TheHerders@users.noreply.github.com>
2024-03-20 09:47:39 -0700

    Updating assignments/string-recursion/StringRecursion.java

--- assignments/string-recursion/StringRecursion.java
+++ assignments/string-recursion/StringRecursion.java
@@ -1,8 +1,10 @@
 public class StringRecursion {
   public int length(String word){
+    String lessWord = word.substring(1);
     if (word.isEmpty()){
       return 0;
+    } else {
+      return lessWord.length();
     }
-    return 1;
   }
 }
