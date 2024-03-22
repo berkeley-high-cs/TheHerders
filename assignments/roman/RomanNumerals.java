98196c67c821a03fa9da5004b8529db3350d81fd
StevenDen <119467873+TheHerders@users.noreply.github.com>
2024-03-22 09:53:55 -0700

    Updating assignments/roman/RomanNumerals.java

--- assignments/roman/RomanNumerals.java
+++ assignments/roman/RomanNumerals.java
@@ -10,12 +10,13 @@ public class RomanNumerals {
     return ROMAN_VALUES[ROMAN_DIGITS.indexOf(s)];
   }
   public int decode(String num){
-    
+     String lessNum = num.substring(1);
+     int firstDigit = num.substring(0,1);
     if (num.isEmpty()){
       return 0;
     } else {
-      String lessNum = num.substring(1);
-      return fromRomanDigit(lessNum) + decode(lessNum);
+     
+      return fromRomanDigit(firstDigit) + decode(lessNum);
     }
   }
 
