c19e5a3d321e94c1ae0a4b79ea147feed040b8cb
StevenDen <119467873+TheHerders@users.noreply.github.com>
2024-03-25 10:59:50 -0700

    Updating assignments/quicksort/Quicksort.java

--- assignments/quicksort/Quicksort.java
+++ assignments/quicksort/Quicksort.java
@@ -1,6 +1,11 @@
 import java.util.*;
 
 public class Quicksort {
-
+  public ArrayList<Integer> quicksort(ArrayList<Integer> list){
+    int pivot = list.get(0);
+    ArrayList<Integer> smallList = new ArrayList<>();
+    ArrayList<Integer> bigList = new ArrayList<>();
+    return list;
+  }
 
 }
