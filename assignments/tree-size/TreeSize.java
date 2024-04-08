6255c30c5da45be3fc945126949b0a616b725c73
StevenDen <119467873+TheHerders@users.noreply.github.com>
2024-04-08 11:04:20 -0700

    Updating assignments/tree-size/TreeSize.java

--- assignments/tree-size/TreeSize.java
+++ assignments/tree-size/TreeSize.java
@@ -1,5 +1,13 @@
 import java.util.*;
 
 public class TreeSize {
-
+  public int recursive(Tree tree){
+    if (tree.children().isEmpty()){
+      return tree.size();
+    } else {
+      for (int i = 0; i < tree.children().size()){
+        return recursive(tree.children().get(i));
+      }
+    }
+  }
 }
