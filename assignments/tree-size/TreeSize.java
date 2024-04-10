3f6d7812c17b7c0362589bcaee7e8b82d7619c0b
StevenDen <119467873+TheHerders@users.noreply.github.com>
2024-04-10 10:22:47 -0700

    Updating assignments/tree-size/TreeSize.java

--- assignments/tree-size/TreeSize.java
+++ assignments/tree-size/TreeSize.java
@@ -15,54 +15,42 @@ public class TreeSize {
   }
 
   public int iterative(Tree tree) {
-    System.err.println("/////////////////////////////////");
     ArrayList<Tree> forest = new ArrayList<>();
     int totalTree = 0;
     if (tree.children().isEmpty()) {
       return tree.size();
     }
     forest.add(tree);
-    for (int i = 0; i < forest.get(i).children().size(); i++) {
-      forest.add(tree.children().get(i));
-
-      while (!forest.get(forest.size() - 1).children().isEmpty()) {
-        for (int x = 0; x < forest.get(forest.size() - 1).children().size(); x++) {
-          forest.add(forest.size(),forest.get(forest.size() - 1).children().get(x));
-         
-        }
-       
-       
+    for (int i = 0; i < forest.size(); i++) {
+      for (int i = 0; i < forest.get(i).children().size(); i++) {
+        forest.add(tree.children().get(i));
       }
     }
-     printArray(forest);
     for (int x = 0; x < forest.size(); x++) {
       totalTree += forest.get(x).size();
     }
     return totalTree;
   }
-   public void printArray(ArrayList<Tree> list){
-      for (int i = 0; i < list.size(); i++){
-          System.err.println(list.get(i).size());
-      }
+
+  public void printArray(ArrayList<Tree> list) {
+    for (int i = 0; i < list.size(); i++) {
+      System.err.println(list.get(i).size());
+    }
   }
 }
-//
-//   ArrayList<Tree> forest = new ArrayList<>();
-//     int totalTree = 0;
-//     if (tree.children().isEmpty()) {
-//       return tree.size();
+// System.err.println("/////////////////////////////////");
+// ArrayList<Tree> forest = new ArrayList<>();
+// int totalTree = 0;
+// if (tree.children().isEmpty()) {
+//   return tree.size();
+// }
+// forest.add(tree);
+// for (int i = 0; i < tree.children().size(); i++) {
+//   forest.add(tree.children().get(i));
+//   while (!forest.get(forest.size() - 1).children().isEmpty()) {
+//     for (int x = 0; x < forest.get(forest.size() - 1).children().size(); x++) {
+//       forest.add(forest.size(),forest.get(forest.size() - 1).children().get(x));
 //     }
-//     for (int i = 0; i < tree.children().size(); i++) {
-//       forest.add(tree.children().get(i));
-//       if (!forest.get(i).children().isEmpty()) {
-//         while (!forest.get(forest.size() - 1).children().isEmpty()) {
-//           for (int y = 0; y < forest.get(forest.size() - 1).children().size(); y++) {
-//             forest.add(forest.get(forest.size() - 1).children().get(y));
-//           }
-//         }
-//       }
-//     }
-//     for (int x = 0; x < forest.size(); x++) {
-//       totalTree += forest.get(x).size();
-//     }
-//     return totalTree;
+//   }
+// }
+//  printArray(forest);
